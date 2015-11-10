75c6c7be4804d61e1c82d1d61a4624f68714672d
LuddeW <ludwig.wiktorsson@gmail.com>
2015-11-10 15:47:36 +0100

    Added Git.ignores and printing out penguin from class

---
+++ SuperPengoMan/SuperPengoMan/GameObject/GameObject.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperPengoMan.GameObject
+{
+    class GameObject
+    {
+        public Texture2D texture;
+        public Vector2 pos;
+        public GameObject(Texture2D texture, Vector2 pos)
+        {
+            this.texture = texture;
+            this.pos = pos;
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, pos, Color.White);
+        }
+    }
+}
---
+++ SuperPengoMan/SuperPengoMan/GameObject/Pengo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperPengoMan.GameObject
+{
+    class Pengo:GameObject
+    {
+        Rectangle srcRect;
+        public Pengo(Texture2D texture, Vector2 pos) : base(texture, pos)
+        {
+            srcRect = new Rectangle(0, 0, 40, 40); 
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, pos, srcRect, Color.White);
+        }
+    }
+}
